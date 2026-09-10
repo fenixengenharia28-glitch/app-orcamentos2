33b3b565ddf456196797bd6c8c08bfb89814c866
fenixengenharia28-glitch <fenixengenharia28@gmail.com>
2026-09-10 12:08:06 -0300

    Update app_orcamento_v10.py

--- app_orcamento_v10.py
+++ app_orcamento_v10.py
@@ -193,4 +193,4 @@ with aba_orc:
     
     valor_servico = 0.0
     if tipo_cobranca == "Por Empreitada / Ponto":
-        preco_sugerido_base = st.session_state.db_servicos[st.session_state.db_servicos["Serviço"] == servico_selecionado]["Preço Padrão"].values col_srv1, col_srv2 = st.columns(2)
+        preco_sugerido_base = st.session_state.db_servicos[st.session_state.db_servicos["Serviço"] == servico_selecionado]["Preço Padrão"].valuescol_srv1, col_srv2 = st.columns(2)
